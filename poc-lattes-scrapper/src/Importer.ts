194973f8b3e3bd8d7bf077811415e7728ea52c48
Gustavo Candido <gustavo.candido@pipefy.com>
2022-06-12 15:46:03 -0300

    feat: get fields of search

--- poc-lattes-scrapper/src/Importer.ts
+++ poc-lattes-scrapper/src/Importer.ts
@@ -1,4 +1,5 @@
 import { readXML } from "./xmlparser";
+import jp from "jsonpath";
 
 class Importer {
   private lattes: any;
@@ -12,42 +13,20 @@ class Importer {
     this.infos = {
       ...this.infos,
       "NOME-COMPLETO":
-        this.lattes["CURRICULO-VITAE"]["DADOS-GERAIS"]["NOME-COMPLETO"],
+        this.lattes?.["CURRICULO-VITAE"]?.["DADOS-GERAIS"]?.["NOME-COMPLETO"],
     };
     return this;
   }
 
   getFieldOfSearch() {
-    const researchsFields = [];
-
-    for (let workActuation of this.lattes?.["CURRICULO-VITAE"]?.[
-      "DADOS-GERAIS"
-    ]?.["ATUACOES-PROFISSIONAIS"]?.["ATUACAO-PROFISSIONAL"]) {
-      if (
-        !workActuation?.["ATIVIDADES-DE-PESQUISA-E-DESENVOLVIMENTO"]?.[
-          "PESQUISA-E-DESENVOLVIMENTO"
-        ]
-      )
-        continue;
-      for (let researchs of workActuation[
-        "ATIVIDADES-DE-PESQUISA-E-DESENVOLVIMENTO"
-      ]?.["PESQUISA-E-DESENVOLVIMENTO"]) {
-        if (!researchs?.["LINHA-DE-PESQUISA"]) continue;
-        if (Array.isArray(researchs?.["LINHA-DE-PESQUISA"])) {
-          for (let research of researchs?.["LINHA-DE-PESQUISA"]) {
-            researchsFields.push(research?.["TITULO-DA-LINHA-DE-PESQUISA"]);
-          }
-        } else {
-          researchsFields.push(
-            researchs?.["LINHA-DE-PESQUISA"]?.["TITULO-DA-LINHA-DE-PESQUISA"]
-          );
-        }
-      }
-    }
+    const researchesFields = jp.query(
+      this.lattes,
+      "$..['TITULO-DA-LINHA-DE-PESQUISA']"
+    );
 
     this.infos = {
       ...this.infos,
-      "LINHA-DE-PESQUISA": researchsFields,
+      "LINHA-DE-PESQUISA": researchesFields,
     };
 
     return this;
