c0188716de2861d0b0e8a67146740b65f219d282
Gustavo Candido <gustavo.candido@pipefy.com>
2022-07-10 22:27:13 -0300

    refactor: utils qualisScore

--- poc-lattes-scrapper/src/utils/qualisScoreRestrict.ts
+++ poc-lattes-scrapper/src/utils/qualisScoreRestrict.ts
@@ -1,13 +1,7 @@
+import isQualisRestrict from "./isQualisRestrict";
 import qualisScore from "./qualisScore";
 
-const qualisScoreRestrict = (qualis: string): number => {
-  const restrictsQualis = ["A1", "A2", "A3", "A4"];
-
-  if (!restrictsQualis.includes(qualis)) {
-    return 0;
-  }
-
-  return qualisScore(qualis);
-};
+const qualisScoreRestrict = (qualis: string): number =>
+  isQualisRestrict(qualis) ? qualisScore(qualis) : 0;
 
 export default qualisScoreRestrict;
